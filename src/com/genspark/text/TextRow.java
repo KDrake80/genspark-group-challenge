26dc6b23fec5148f61aca1d351e2ed8c38a063d5
Alonso del Arte <alonso.delarte@gmail.com>
2022-07-11 18:10:38 -0400

    Passing first test for TextRow

--- src/com/genspark/text/TextRow.java
+++ src/com/genspark/text/TextRow.java
@@ -3,7 +3,10 @@ package com.genspark.text;
 public class TextRow {
 
     public TextRow(String firstColumnText) {
-        //
+        if (firstColumnText == null) {
+            String excMsg = "First column text must not be null";
+            throw new NullPointerException(excMsg);
+        }
     }
 
 }
