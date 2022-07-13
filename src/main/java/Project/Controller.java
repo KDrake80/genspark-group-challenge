ffd9592cc632b3b0e993942ef5a3749541892682
Kevin <Kevindrake80@gmail.com>
2022-07-13 08:57:07 -0600

    Finished GUI
    GUI is completed. As well as the updated FXML doc, Controller Class, and RunApplication class.

--- src/main/java/Project/Controller.java
+++ src/main/java/Project/Controller.java
@@ -1,40 +1,36 @@
 package Project;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
-
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     List<String> sortedList;
+    ArrayList<Text> nodes;
     @FXML
     GridPane gridPane;
-
     @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        nodes = new ArrayList<>();
         try {
             sortedList = SortInput.sortInput().stream().toList();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        gridPane.add(new Text(sortedList.get(0)), 0, 0);
-        for (int i = 0; i < sortedList.size(); i++) {
-            gridPane.add(new Text(sortedList.get(i)), 0, i);
-            if (i >= 10) {
-                gridPane.add(new Text(sortedList.get(i)), 1, (i - 10));
+            for (String s:sortedList) {
+                nodes.add(new Text("\t" + s));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         gridPane.setGridLinesVisible(true);
+        gridPane.setPadding(new Insets(5, 5, 5,5));
+        for (int i = 0; i < 10; i++) {
+            gridPane.addRow(i, nodes.get(i), nodes.get(i + 10));
+        }
     }
 }
