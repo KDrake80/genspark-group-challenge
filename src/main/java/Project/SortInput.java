f86dedfa4bfdc3eb9e0ca87e69899feb38378961
itMeansBigMountain <affan.fareed@gmail.com>
2022-07-15 10:21:05 -0500

    SortInput() uses parallel stream now for faster execution

--- src/main/java/Project/SortInput.java
+++ src/main/java/Project/SortInput.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.ExecutorService;
 
 public class SortInput {
     public static ArrayList<String> sortInput() throws IOException {
@@ -19,10 +20,27 @@ public class SortInput {
         //sort int array and string array together
         BufferedReader br = new BufferedReader(new FileReader("TextInput"));
         String line;
+
+
+//        while ((line = br.readLine()) != null) {
+//            list.add(line);
+//            intList.add(Extract_Int_Value.extract_html(line));
+//        }
+
         while ((line = br.readLine()) != null) {
             list.add(line);
-            intList.add(Extract_Int_Value.extract_html(line));
         }
+        list.parallelStream().forEach(
+                (l) -> {
+                    try {
+                        intList.add(Extract_Int_Value.extract_html(l));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+        System.out.println(intList);
+
 
         int total = intList.get(0);
         for (int i = 1; i < intList.size(); i++) {
@@ -30,16 +48,26 @@ public class SortInput {
             if (intList.get(i - 1) > intList.get(i)) {
                 Collections.swap(intList, i - 1, i);
                 Collections.swap(list, i - 1, i);
-                for (int y = i-1 ; y > 0; y--)
-                {
-                    if (intList.get(y-1) > intList.get(y))
-                    {
-                        Collections.swap(intList , y-1,y);
-                        Collections.swap(list , y-1,y);
+                for (int y = i - 1; y > 0; y--) {
+                    if (intList.get(y - 1) > intList.get(y)) {
+                        Collections.swap(intList, y - 1, y);
+                        Collections.swap(list, y - 1, y);
                     }
                 }
             }
         }
         return list;
     }
+
+
+    public static void main(String[] args) throws IOException {
+        long startTime = System.nanoTime();
+        sortInput();
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+        System.out.println(duration);
+    }
+
+
 }
